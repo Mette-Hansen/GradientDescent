a027f053220fa7f339c98ba659bc3d3c25a74101
Mette Hansen <meha419@student.sdu.dk>
2021-12-15 18:15:22 +0100

    hate this

--- Assignment2.py
+++ Assignment2.py
@@ -1,12 +1,11 @@
 import numpy as np
 # Initialize parameters:
-x_0 = 3  # The algorithm starts at 3
-step_size = 0.01  # Learning rate
+x_0 = 5 # The algorithm starts at 3
+step_size = 0.2  # Learning rate
 count = 0  # Iteration counter
-def df(x): return 5*np.exp(-(np.power(x, 2)/8))*x  # Gradient of the function
+df = lambda x: 5*np.exp(-(np.power(x, 2)/8))*x+np.pi*np.exp(0.5*np.cos(2*np.pi*x))*np.sin(2*np.pi*x)  # Gradient of the function
 
-
-while df(x_0) > np.finfo(float).eps and count < 1000:
+while df(x_0) > np.finfo(float).eps and count < 100:
     x_0 = x_0 - step_size*df(x_0)  # Gradient descent
     count = count + 1
     print("Iteration", count, "\nX value is", df(x_0))
